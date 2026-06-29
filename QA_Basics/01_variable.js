bb6a7cc1714abebe7b8b2f75d1678543415461a0
TurkaneVaishali <157511250+TurkaneVaishali@users.noreply.github.com>
2026-06-29 09:35:19 +0000

    datatypes

---
+++ QA_Basics/01_variable.js
@@ -0,0 +1,4 @@
+const accountId = 144553
+let accountEmail = "turkanesonu956@gmail.com"
+var accountPassword = "Tuhi@1509"
+accountCity = "Jaipur"
---
+++ QA_Basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version // 
+// alert(3+3) // we are using node js not browser. 
+	// alert(3+3) // we are using node js not browser.
+	Datatypes -> // number => 2 to power pf 53
+	// bigint
+	// string => “”
+	// Boolean => true / false
+	// null – it is a representation of emplty value. 
+	// undefined – value defined nahi kiya hai. 
+	// symbol => unique
+	// object
+	Console.log(typeof”Seoul”);
+	Console.log( typeof undefined); // undefined
+	Console.log( typeof null); // object
