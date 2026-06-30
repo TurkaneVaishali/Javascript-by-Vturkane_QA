f0eb1cfba30fbca6cd6e5f6674348de606140253
TurkaneVaishali <157511250+TurkaneVaishali@users.noreply.github.com>
2026-06-30 12:17:12 +0000

    arrays

---
+++ 02_Basics/arrays.js
@@ -0,0 +1,7 @@
+// array
+
+//const myArr = [0,1,2,3,4,5]
+// Javascript arrays are resizable, it can be add or delet the element. Mix data is accepted. 
+// array indexing start with 0. 
+let arr = [10, 20, 30];
+console.log(arr[3]);
