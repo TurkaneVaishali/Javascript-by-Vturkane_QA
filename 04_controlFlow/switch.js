a9499dde00daaef3d51763f27ac8e45c92cfaf56
TurkaneVaishali <157511250+TurkaneVaishali@users.noreply.github.com>
2026-07-08 10:55:12 +0000

    truthy

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,25 @@
+// // switch (key){
+// //     case.value:
+// //         break;
+
+// //         default:
+// //             break;
+// // }
+
+// const month = 3
+
+// switch (month) {
+//     case "jan":
+//         console.log("January");
+        
+//         break;
+//     case "feb":
+//         console.log("February");
+//         break;
+//     case "March":
+//         console.log("March");
+
+//     default:
+//         break;
+// }
+
