3dee6cbd27796ae24680ed0e0e2e91906e1951c7
TurkaneVaishali <157511250+TurkaneVaishali@users.noreply.github.com>
2026-07-08 18:57:43 +0000

    filterMapReduce

--- iterations/forEachLoop.js
+++ iterations/forEachLoop.js
@@ -1,42 +1,51 @@
-const coding = ["js", "ruby", "java", "JS", "cpp", "c++"]
+// const coding = ["js", "ruby", "java", "JS", "cpp", "c++"]
 
-// coding.forEach(function(val){
-//     console.log(val);
+// // coding.forEach(function(val){
+// //     console.log(val);
     
-// }) // Foreach loop.
+// // }) // Foreach loop.
 
-// coding.forEach((item) => {
-//     console.log(item);
+// // coding.forEach((item) => {
+// //     console.log(item);
     
-// // }
-// // )
-// // arrow function syntax shortcut -- 
-// // (()=>{})
+// // // }
+// // // )
+// // // arrow function syntax shortcut -- 
+// // // (()=>{})
 
-//     coding.forEach((item, index, arr)=> {
-//         console.log(item, index, arr);
+// //     coding.forEach((item, index, arr)=> {
+// //         console.log(item, index, arr);
         
-//     })
-//===================================================================
+// //     })
+// //===================================================================
 
-const myCoding = [ // array ke under object hai usaka access lena. 
-    {
-        languageName: "javascript",
-        languageFileName : ".js"
-    } , 
-    {
-        languageName: "python",
-        languageFileName : ".py"
-    } ,
-    {
-        languageName: "java",
-        languageFileName : ".java"
-    }
+// const myCoding = [ // array ke under object hai usaka access lena. 
+//     {
+//         languageName: "javascript",
+//         languageFileName : ".js"
+//     } , 
+//     {
+//         languageName: "python",
+//         languageFileName : ".py"
+//     } ,
+//     {
+//         languageName: "java",
+//         languageFileName : ".java"
+//     }
 
 
-]
+// ]
 
-myCoding.forEach((item)=>{
-    console.log(item.languageFileName, item.languageName);
+// myCoding.forEach((item)=>{
+//     console.log(item.languageFileName, item.languageName);
+    
+// })
+
+
+const myNum = [1,2,3,4,5,6,7,8,9,10]
+
+const newNum = myNum.filter((num)=> {
+    return num > 4
     
 })
+console.log(newNum);
