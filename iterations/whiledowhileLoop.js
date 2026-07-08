73efa0755b82069c23241cd8ba58dea853bc90c1
TurkaneVaishali <157511250+TurkaneVaishali@users.noreply.github.com>
2026-07-08 12:28:42 +0000

    whiledowhileloops

---
+++ iterations/whiledowhileLoop.js
@@ -0,0 +1,25 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of an index is ${index}`);
+//     index = index + 2
+    
+// }
+
+// while loop with arrays
+// let myArray = ["flash", "batman" , "superman"]
+// let arr = 0 // taken a variable to declare or to start the loop.
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+    
+// }
+//==========================================================================================
+
+// # Do while loop :-> 
+let score = 1
+do {                        // first condition will get check then the condion will apply or will get chek. 
+    console.log(`score is ${score}`);
+    score ++ 
+    
+} while (score <= 10);
